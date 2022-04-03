f58078b8a5a7a99ca8f9ab7cf2999c755a1ce11c
Existence.exe <pritishdutta20@gmail.com>
2022-04-04 01:42:03 +0530

    boilerplate

---
+++ back-end/index.js
@@ -0,0 +1,17 @@
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+let win;
+
+app.on('ready', function(){
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        icon: './resources/spotify-512.png'
+    });
+
+    win.loadFile('./front-end/index.html');
+});
